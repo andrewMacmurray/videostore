6c3da0bd4daf476d3ec8c1fc1d451b29aad0f7f8
andrewMacmurray <a.macmurray@icloud.com>
2018-05-14 14:53:43 +0100

    adds assertStatement helper

--- src/test/java/VideoStoreTest.java
+++ src/test/java/VideoStoreTest.java
@@ -1,6 +1,8 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.assertEquals;
 
 public class VideoStoreTest {
@@ -8,7 +10,8 @@ public class VideoStoreTest {
     private CustomerStatement customerStatement;
 
     private final Movie theCell = new NewReleaseMovie("The Cell");
-    private final Movie tiggerMovie = new NewReleaseMovie("The Tigger Movie");
+    private final Movie meanGirls = new NewReleaseMovie("Mean Girls");
+    private final Movie tiggerMovie = new ChildrensMovie("The Tigger Movie");
     private final Movie plan9 = new RegularMovie("Plan 9 from Outer Space");
     private final Movie eraserHead = new RegularMovie("Eraserhead");
     private final Movie eightAndAHalf = new RegularMovie("8 1/2");
@@ -22,41 +25,25 @@ public class VideoStoreTest {
     public void testSingleNewReleaseStatement() {
         addRental(theCell, 3);
 
-        String expected = printedLines(
-                "Rental Record for Fred",
-                tabbed("The Cell", "9.0"),
-                "You owed 9.0",
-                "You earned 2 frequent renter points"
-        );
-        assertEquals(expected, customerStatement.statement());
+        assertStatement("9.0", "2", movie("The Cell", "9.0"));
     }
 
     @Test
     public void testDualNewReleaseStatement() {
         addRental(theCell, 3);
-        addRental(tiggerMovie, 3);
+        addRental(meanGirls, 3);
 
-        String expected = printedLines(
-                "Rental Record for Fred",
-                tabbed("The Cell", "9.0"),
-                tabbed("The Tigger Movie", "9.0"),
-                "You owed 18.0",
-                "You earned 4 frequent renter points"
+        assertStatement("18.0", "4",
+                movie("The Cell", "9.0"),
+                movie("Mean Girls", "9.0")
         );
-        assertEquals(expected, customerStatement.statement());
     }
 
     @Test
     public void testSingleChildrensStatement() {
         addRental(tiggerMovie, 3);
 
-        String expected = printedLines(
-                "Rental Record for Fred",
-                tabbed("The Tigger Movie", "1.5"),
-                "You owed 1.5",
-                "You earned 1 frequent renter points"
-        );
-        assertEquals(expected, customerStatement.statement());
+        assertStatement("1.5", "1", movie("The Tigger Movie", "1.5"));
     }
 
     @Test
@@ -65,27 +52,36 @@ public class VideoStoreTest {
         addRental(eightAndAHalf, 2);
         addRental(eraserHead, 3);
 
-        String expected = printedLines(
+        assertStatement("7.5", "3",
+                movie("Plan 9 from Outer Space", "2.0"),
+                movie("8 1/2", "2.0"),
+                movie("Eraserhead", "3.5")
+        );
+    }
+
+    private void addRental(Movie movie, int daysRented) {
+        customerStatement.addRental(new Rental(movie, daysRented));
+    }
+
+    private void assertStatement(String totalAmount, String frequentPoints, String... movieLines) {
+        String expected = newLines(
                 "Rental Record for Fred",
-                tabbed("Plan 9 from Outer Space", "2.0"),
-                tabbed("8 1/2", "2.0"),
-                tabbed("Eraserhead", "3.5"),
-                "You owed 7.5",
-                "You earned 3 frequent renter points"
+                newLines(movieLines) + "You owed " + totalAmount,
+                "You earned " + frequentPoints + " frequent renter points"
         );
         assertEquals(expected, customerStatement.statement());
+    }
+
+    private String movie(String title, String cost) {
+        return tabbed(title, cost);
     }
 
     private String tabbed(String... lines) {
         return "\t" + String.join("\t", lines);
     }
 
-    private String printedLines(String... lines) {
+    private String newLines(String... lines) {
         return String.join("\n", lines) + "\n";
-    }
-
-    private void addRental(Movie movie, int daysRented) {
-        customerStatement.addRental(new Rental(movie, daysRented));
     }
 
 }
